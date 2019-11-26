2637c083c94f49c0e26fcaa5422fc09c5af12292
Yordan RADEV <yradev@lab1-1.CS.McGill.CA>
2019-11-26 14:54:11 -0500

    mimi public html

---
+++ cgi-bin/answer.c
@@ -0,0 +1,48 @@
+// Student Name : Yordan Radev
+// Student ID   : 260 744 314
+
+#include <stdio.h>
+#include <stdlib.h>
+
+//char *stringSubmit = getenv(“QUERY__STRING”);
+/*
+void checkGold(int gold)
+{
+	if(gold == 0)
+	{
+		printf("YOU LOSE");
+	} else if (gold == 100)
+	{
+		printf("YOU WIN");
+	}
+}
+*/
+int main(void) {
+printf( "Content-type: text/html\n\n");
+printf("<html>");
+printf("<head><title>MY TITLE</title></head>");
+printf("<body><p>Unable to open file!</p></body>");
+printf("</html>");
+/*
+switch(&stringSubmit)
+{
+	case 'NORTH':
+		printf(“<a href="https://www.cs.mcgill.ca/~???">NORTH</a>”);
+	case 'SOUTH':
+		printf(“<a href="https://www.cs.mcgill.ca/~???">SOUTH</a>”);
+	case 'EAST':
+		printf(“<a href="https://www.cs.mcgill.ca/~???">EAST</a>”);
+	case 'WEST':
+		printf(“<a href="https://www.cs.mcgill.ca/~???">WEST</a>”);
+	case 'GOLD':
+		printf(“<a href="https://www.cs.mcgill.ca/~yradev/COMP-206-Ass-7.html">GOLD</a>”);
+	case '5':
+		correct answer
+	default:
+		not a direction or gold or answer
+}
+
+*/
+return 0 ;
+
+}
