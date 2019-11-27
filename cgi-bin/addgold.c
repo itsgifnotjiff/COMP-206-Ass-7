d1ebdeca5457d03be5db07ed3a7e8aafe311ed29
Yordan RADEV <yradev@cs-26.CS.McGill.CA>
2019-11-27 14:34:10 -0500

    addgold.c addition

--- cgi-bin/addgold.c
+++ cgi-bin/addgold.c
@@ -1,2 +1,39 @@
 // Student Name : Yordan Radev
 // Student ID   : 260 744 314
+
+#include <stdio.h>
+#include <stdlib.h>
+
+<input type=”hidden” name=”gold” value=”10”>
+
+
+printf(“<!DOCTYPE html>");
+printf(“<html>");
+printf(“<head>");
+printf(“<title> COMP 206 Assignment 7 </title>");
+printf(“<link rel="icon" href="http://i58.tinypic.com/302rtyg.png" type="image/png">");
+printf(“</head>");
+printf(“<body>");
+printf(“<center>");
+printf(“<h1 style="text-align:center">Yggdrasil</h1>");
+printf("<img src="tree.jpg" align="middle" width="768" height="540">");
+printf(“<button type="button" onclick="alert('Alexis Dubois uses scotchtape to masturbate!!!')">Click Me!</button>");
+printf(“<h2>Please type NORTH, SOUTH, EAST, WEST, GOLD, or answer the question.</h2>");
+printf(“<p>What is the minimal number of git commits required to prove you understand how to do it? (Hint: 5)</p>");
+printf(“<form action="cgi-bin/answer" method="get">");
+printf(“Answer : <br>");
+printf(“<input type="text" name="answer" value="GOLD!!!">");
+printf(“<br>");
+printf(“<input type="submit" value="Submit">");
+printf(“</form>");
+printf(“</center>");
+printf(“</body> ");
+printf(“</html>");
+
+
+
+
+
+
+printf(“<input type=\”hidden\” name=\”gold\”
+value=\”%d\””, goldpieces);
