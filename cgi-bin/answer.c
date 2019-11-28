40c94851face243b79ca21a011df5a8ca7d2a876
Yordan RADEV <yradev@lab2-4.CS.McGill.CA>
2019-11-28 13:51:02 -0500

    one tutorial later

--- cgi-bin/answer.c
+++ cgi-bin/answer.c
@@ -4,7 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//char *stringSubmit = getenv(“QUERY__STRING”);
+
 /*
 void checkGold(int gold)
 {
@@ -17,17 +17,47 @@ void checkGold(int gold)
 	}
 }
 */
-int main(void) {
-printf( "Content-type: text/html\n\n");
+void main() {
+    
+char array[100];
+    
+char *data = getenv("QUERY_STRING");
+// data = getenv(QUERY_STRING);
+
+printf("syvif nvb");
+/*
+char *stringSubmit = getenv(“QUERY_STRING”);
+data = getenv(QUERY_STRING);
+
+token = strtok(array, "?");
+while(token != NULL)
+{
+ token = strtok(NULL, "?");
+ fprintf(text, 
+}
+*/
+
+
+/*
+printf("Content-type: text/html\n\n");
 printf("<html>");
 printf("<head><title>MY TITLE</title></head>");
-printf("<body><p>Unable to open file!</p></body>");
+printf("<body><p>Something</p></body>");
 printf("</html>");
+*/
+
+
+
 /*
 switch(&stringSubmit)
 {
 	case 'NORTH':
-		printf(“<a href="https://www.cs.mcgill.ca/~???">NORTH</a>”);
+		//printf(“<a href="https://www.cs.mcgill.ca/~???">NORTH</a>”);
+        printf( "Content-type: text/html\n\n");
+        printf("<html>");
+        printf("<head><title>MY TITLE</title></head>");
+        printf("<body><p>Something </p></body>");
+        printf("</html>");
 	case 'SOUTH':
 		printf(“<a href="https://www.cs.mcgill.ca/~???">SOUTH</a>”);
 	case 'EAST':
@@ -40,9 +70,9 @@ switch(&stringSubmit)
 		correct answer
 	default:
 		not a direction or gold or answer
-}
 
+}
 */
-return 0 ;
+
 
 }
