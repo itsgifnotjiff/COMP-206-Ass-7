bb67498b3d469ef84ffdf248b4f90af480296b22
Yordan RADEV <yradev@teaching.CS.McGill.CA>
2019-12-01 14:45:36 -0500

    now that mimi is up here is the final version

--- addgold.c
+++ addgold.c
@@ -1,39 +1,40 @@
-// Student Name : Yordan Radev
-// Student ID   : 260 744 314
+// Sudent Name : Yordan Radev
+// Student ID  : 260 744 314
 
 #include <stdio.h>
 #include <stdlib.h>
-
-<input type=”hidden” name=”gold” value=”10”>
-
-
-printf(“<!DOCTYPE html>");
-printf(“<html>");
-printf(“<head>");
-printf(“<title> COMP 206 Assignment 7 </title>");
-printf(“<link rel="icon" href="http://i58.tinypic.com/302rtyg.png" type="image/png">");
-printf(“</head>");
-printf(“<body>");
-printf(“<center>");
-printf(“<h1 style="text-align:center">Yggdrasil</h1>");
-printf("<img src="tree.jpg" align="middle" width="768" height="540">");
-printf(“<button type="button" onclick="alert('Alexis Dubois uses scotchtape to masturbate!!!')">Click Me!</button>");
-printf(“<h2>Please type NORTH, SOUTH, EAST, WEST, GOLD, or answer the question.</h2>");
-printf(“<p>What is the minimal number of git commits required to prove you understand how to do it? (Hint: 5)</p>");
-printf(“<form action="cgi-bin/answer" method="get">");
-printf(“Answer : <br>");
-printf(“<input type="text" name="answer" value="GOLD!!!">");
-printf(“<br>");
-printf(“<input type="submit" value="Submit">");
-printf(“</form>");
-printf(“</center>");
-printf(“</body> ");
-printf(“</html>");
+#include <string.h>
 
 
+void main() {
+	
+	// Payload
+	char *data = getenv("QUERY_STRING");
+	strtok(data, "=");
+	
+	int gold = atoi(strtok(NULL,""));
+	
+	printf("Content-Type:text/html\n\n");
+	printf("<html>");
+	printf("<head> </head>");
+	printf("<body>");
 
 
+	FILE *p = fopen("../index.html","rt");
+	if(p==NULL) printf("Error trying to open the index.html file");
+	char line[1000];
+	while(fgets(line,999,p)){
+		char tag[20];
+		sscanf(line, "%s", tag);
+		if(strcmp(tag, "<form")==0){
+			printf("<form action=\"answer.cgi\" method=\"get\">\n");
+			printf("<input type=\"hidden\" name=\"gold\" value=\"%d\">",gold);
+		} else {
+			printf("%s", line);
+		}	
 
+	}
 
-printf(“<input type=\”hidden\” name=\”gold\”
-value=\”%d\””, goldpieces);
+	fclose(p);
+
+}
--- answer.c
+++ answer.c
@@ -1,58 +1,67 @@
 // Student Name : Yordan Radev
 // Student ID   : 260 744 314
 
-#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 
-int main() {
-    
-char north[]="NORTH",south[]="SOUTH", east[]="EAST", west[]="WEST", gold[]="GOLD", correctAnswer[]="5";    
-char array[100];
-    
-char *data = getenv("QUERY_STRING");
-// data = getenv(QUERY_STRING);
+void main() {
+	
+	// Payload Get
+	char *data = getenv("QUERY_STRING");
+	
 
-printf("syvif nvb");
+	// Tokenize 
+	
+	char *tokOne;
+	int goldCoins;
+	char *command;
+	tokOne = strtok( data, "=" );
+	if( strcmp(tokOne,"answer") == 0 ){
+		command = strtok( NULL, "&" );
+		if( strtok( NULL, "=" ) == NULL ) goldCoins = 10;
+		else goldCoins = atoi( strtok( NULL, "" ) );
+	} else {
+		goldCoins = atoi( strtok( NULL, "&" ) );
+		if( strtok( NULL, "=" ) != NULL ) command = strtok( NULL, "" );
+	}
+	
+	printf("Content-Type:text/html\n\n");
+	printf("<html>");
+	printf("<body>");
 
-if (strcmp(data,north) == 0)
-{
-    printf(<html><a href="https://www.cs.mcgill.ca/~bemili">Press HERE to go NORTH</a></html>);
-} else if (strcmp(data,south) == 0)
-{
-    printf(<html><a href="https://www.cs.mcgill.ca/~bemili">Press HERE to go SOUTH</a></html>);
-} else if (strcmp(data,east) == 0)
-{
-    printf(<html><a href="https://www.cs.mcgill.ca/~bemili">Press HERE to go EAST</a></html>);
-} else if (strcmp(data,west) == 0)
-{
-    printf(<html><a href="https://www.cs.mcgill.ca/~bemili">Press HERE to go WEST</a></html>);
-} else if (strcmp(data,gold) == 0)
-{
-    printf("GOLD");
-}
+	if ( strcmp( command , "NORTH" ) == 0 ) {
+		printf("<a href=\"http://cs.mcgill.ca/~anikul3/cgi-bin/addgold.cgi?gold=%d\">Press HERE to go NORTH</a>", goldCoins );
+	} else if ( strcmp( command , "SOUTH" ) == 0 ) {
+		printf("<a href=\"http://cs.mcgill.ca/~aduboi8/cgi-bin/addgold.cgi?gold=%d\">Press HERE to go SOUTH</a>",goldCoins);
+	} else if ( strcmp( command , "EAST" ) == 0 ) {
+		printf("<a href=\"http://cs.mcgill.ca/~ywang498/cgi-bin/addgold.cgi?gold=%d\">Press HERE to go EAST</a>",goldCoins);
+	} else if ( strcmp( command , "WEST" ) == 0 ) {
+		printf("<a href=\"http://cs.mcgill.ca/~ldesch11/cgi-bin/addgold.cgi?gold=%d\">Press HERE to go WEST</a>",goldCoins);
+	} else if ( strcmp( command , "GOLD" ) == 0 ) {
+		 printf("You have %d gold coins.<br /> <a href=\"http://cs.mcgill.ca/~yradev/cgi-bin/addgold.cgi?gold=%d\">Press HERE to go back.</a>",goldCoins,goldCoins);
+		
 
+	} else if ( strcmp( command , "5" ) == 0 ) {
+		goldCoins += 10;
+		printf("Right answer, now you have %d gold coins. <br /><a href=\"http://cs.mcgill.ca/~yradev/cgi-bin/addgold.cgi?gold=%d\">Press HERE to go back. </a>", goldCoins, goldCoins);	
+		if ( goldCoins >= 100 ) {
+			printf("Victory!!! <br /> <a href=\"http://cs.mcgill.ca/~yradev\">Press HERE to start a new game</a>");
+	
+		}
 
-/*
-char *stringSubmit = getenv(“QUERY_STRING”);
-data = getenv(QUERY_STRING);
-
-token = strtok(array, "?");
-while(token != NULL)
-{
- token = strtok(NULL, "?");
- fprintf(text, 
-}
-*/
-
-
-/*
-printf("Content-type: text/html\n\n");
-printf("<html>");
-printf("<head><title>MY TITLE</title></head>");
-printf("<body><p>Something</p></body>");
-printf("</html>");
-*/
-
-
-
+	} else {
+        
+		goldCoins -= 5;
+		printf("Neither a direction nor a right answer. You have %d gold coins left. <br /> <a href=\"http://cs.mcgill.ca/~yradev/cgi-bin/addgold.cgi?gold=%d\">Press HERE to go back.</a>", goldCoins, goldCoins);
+	
+	
+		if(goldCoins <= 0) {
+            
+			printf("Defeat! <br /> <a href=\"https://www.cs.mcgill.ca/~yradev\">Press HERE to start over</a>");
+		}
+	}
+	
+	printf("</body>");
+	printf("</html>");
 }
